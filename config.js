cfaa6ffb32e9db60ed463cd1bcd998268441e0ff
chrischavarria <chrischavarria2@gmail.com>
2026-05-07 14:51:31 -0700

    Update Supabase configuration with new credentials

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.REPLACEMENT_DASHBOARD_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://boidybhvllsaswvhboky.supabase.co",
+  supabaseAnonKey: "sb_publishable_v_l9NzLZIR8RStAhTKsUug_nKWboFZ0"
 };
